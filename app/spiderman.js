ab912cec603d733bf195885c1f04aedeb61adb07
Fernando Cortez <fernandolcortezlgarcia@gmail.com>
2022-04-25 19:13:27 -0500

    Agregando metodo getinfo() con su respetivo test

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,9 +1,13 @@
 class spiderman {
     constructor(name, age, numMovies, schoolcinema) {
-        this.name = name;
-        this.age = age;
-        this.numMovies = numMovies;
-        this.schoolcinema = schoolcinema;
+            this.name = name;
+            this.age = age;
+            this.numMovies = numMovies;
+            this.schoolcinema = schoolcinema;
+        }
+        //metod for get info spiderman and study
+    getInfo() {
+        return `Hey, I'm ${this.name} and from ${this.schoolcinema}`;
     }
 }
 //exportamos la clase
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -2,13 +2,20 @@
 const spiderman = require('./../app/spiderman');
 
 describe("Unit Test for spiderman class", () => {
-    test('1) create an spiderman object', () => {
-        //Escribimos el codigo que queremos usar tal cual
-        const andrewGarfield = new spiderman('Andrew Garfield', 31, 3, 'Sony')
-            //validamos este codigo
-        expect(andrewGarfield.name).toBe('Andrew Garfield')
-        expect(andrewGarfield.age).toBe(31)
-        expect(andrewGarfield.numMovies).toBe(3)
-        expect(andrewGarfield.schoolcinema).toBe('Sony')
+        test('1) create an spiderman object', () => {
+            //Escribimos el codigo que queremos usar tal cual
+            const andrewGarfield = new spiderman('Andrew Garfield', 31, 3, 'Sony')
+                //validamos este codigo
+            expect(andrewGarfield.name).toBe('Andrew Garfield')
+            expect(andrewGarfield.age).toBe(31)
+            expect(andrewGarfield.numMovies).toBe(3)
+            expect(andrewGarfield.schoolcinema).toBe('Sony')
+        })
+    })
+    //test 2 get info spiderman
+describe("Unit Test for spiderman class", () => {
+    test('2) get info spiderman', () => {
+        const tomHolland = new spiderman('Tom Holland', 25, 5, 'Marvel Studios')
+        expect(tomHolland.getInfo()).toBe('Hey, I\'m Tom Holland and from Marvel Studios')
     })
 })
